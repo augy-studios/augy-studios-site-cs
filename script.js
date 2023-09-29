ecb7cb59bad2866021455a0dcbdf4dd5b87927e1
Augy <ateoks@gmail.com>
2023-09-29 23:33:29 +0800

    new home page test 8

--- script.js
+++ script.js
@@ -1,75 +1,31 @@
-document.getElementById("socialsBtn").addEventListener("click", function (event) {
-    let target = event.target;
-
-    while (target !== this) {
-        if (target.classList.contains('box')) {
-            const link = "https://links.augystudios.com/main";
-
-            const newWindow = window.open(link, "_blank");
-
-            if (!newWindow) {
-                window.location.href = link;
-            }
-
-            return;
-        }
-        target = target.parentNode;
+document.getElementById("socialsBtn").addEventListener("click", function(event) {
+    const link = "https://links.augystudios.com/main";
+    const newWindow = window.open(link, "_blank");
+    if (!newWindow) {
+        window.location.href = link;
     }
 });
 
-document.getElementById("docsBtn").addEventListener("click", function (event) {
-    let target = event.target;
-
-    while (target !== this) {
-        if (target.classList.contains('box')) {
-            const link = "https://docs.augystudios.com/";
-
-            const newWindow = window.open(link, "_blank");
-
-            if (!newWindow) {
-                window.location.href = link;
-            }
-
-            return;
-        }
-        target = target.parentNode;
+document.getElementById("docsBtn").addEventListener("click", function(event) {
+    const link = "https://docs.augystudios.com/";
+    const newWindow = window.open(link, "_blank");
+    if (!newWindow) {
+        window.location.href = link;
     }
 });
 
-document.getElementById("expBtn").addEventListener("click", function (event) {
-    let target = event.target;
-
-    while (target !== this) {
-        if (target.classList.contains('box')) {
-            const link = "https://test.augy.xyz/";
-
-            const newWindow = window.open(link, "_blank");
-
-            if (!newWindow) {
-                window.location.href = link;
-            }
-
-            return;
-        }
-        target = target.parentNode;
+document.getElementById("expBtn").addEventListener("click", function(event) {
+    const link = "https://test.augy.xyz/";
+    const newWindow = window.open(link, "_blank");
+    if (!newWindow) {
+        window.location.href = link;
     }
 });
 
-document.getElementById("contactBtn").addEventListener("click", function (event) {
-    let target = event.target;
-
-    while (target !== this) {
-        if (target.classList.contains('box')) {
-            const link = "https://augystudios.com/contact-us";
-
-            const newWindow = window.open(link, "_blank");
-
-            if (!newWindow) {
-                window.location.href = link;
-            }
-
-            return;
-        }
-        target = target.parentNode;
+document.getElementById("contactBtn").addEventListener("click", function(event) {
+    const link = "https://augystudios.com/contact-us";
+    const newWindow = window.open(link, "_blank");
+    if (!newWindow) {
+        window.location.href = link;
     }
 });
