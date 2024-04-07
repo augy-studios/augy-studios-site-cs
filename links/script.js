38a9c8af16e96a4a4257640d5368d3791bfc6d9f
Augy <ateoks@gmail.com>
2024-04-07 18:55:34 +0800

    global links page test 7

--- links/script.js
+++ links/script.js
@@ -18,6 +18,28 @@ function playCSDL() {
     audio.play();
 }
 
+// Get the "backToTop" button
+let backToTopBtn = document.getElementById("backToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 // Get the current year for the copyright footer text
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
