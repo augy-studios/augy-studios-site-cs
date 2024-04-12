99d5ac3aab145e2ec7627dedab6f5111aad416df
Augy <ateoks@gmail.com>
2024-04-12 10:19:05 +0800

    global links page test 48

--- links/queries.js
+++ links/queries.js
@@ -5,6 +5,7 @@ let queryList = [{
         displayText: "How did we get here?",
         extLink: "https://augystudios.com/",
         descHover: "It looks like you got this link without scanning a QR code or being redirected! What kind of sorcery is that?",
+        status: "active",
     },
     {
         name: "Legacy Linktree",
@@ -13,6 +14,7 @@ let queryList = [{
         displayText: "You got here from the Legacy Linktree site!",
         extLink: "https://links.augystudios.com/",
         descHover: "Wow, you're a legacy! Click to get back there.",
+        status: "active",
     },
     {
         name: "GFTV Team ID",
@@ -21,6 +23,7 @@ let queryList = [{
         displayText: "You scanned the QR code on Augy's Global Furry Television (GFTV) Team ID!",
         extLink: "https://globalfurry.tv/",
         descHover: "The GFTV Team ID is a unique identifier of every GFTV Team Member.",
+        status: "active",
     },
     {
         name: "Name Card 4",
@@ -29,6 +32,7 @@ let queryList = [{
         displayText: "You scanned the QR code on Augy's 4th iteration of his Name Card!",
         extLink: "https://augystudios.com/",
         descHover: "A furry name card is a personal creation that displays everything you need to know about the person you met. Augy's 4th name card features his new Telegram sticker pack's sticker.",
+        status: "active",
     },
     {
         name: "LIFC 2024",
@@ -37,6 +41,7 @@ let queryList = [{
         displayText: "You scanned the NFC Tag on Augy's Little Island Furcon (LIFC) 2024 Conbadge!",
         extLink: "https://augystudios.com/",
         descHover: "LIFC is a furry convention held annually in Singapore.",
+        status: "active",
     },
     {
         name: "FurUM 2024",
@@ -45,5 +50,6 @@ let queryList = [{
         displayText: "You scanned the NFC Tag on Augy's Furs Upon Malaysia (FurUM) 2024 Conbadge!",
         extLink: "https://augystudios.com/",
         descHover: "FurUM is a furry convention held annually in Petaling Jaya Malaysia.",
+        status: "inactive",
     },
 ]
