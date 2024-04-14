2ca855c9d2f13fd54aa0714b06be6203e44eab04
Augy <ateoks@gmail.com>
2024-04-14 19:19:17 +0800

    global links page test 68

--- links/queries.js
+++ links/queries.js
@@ -1,55 +1,50 @@
-let queryList = [{
+let queryList = {
+    def: {
         name: "Default",
         source: "none",
-        query: "def",
         displayText: "How did we get here?",
         extLink: "https://augystudios.com/",
         descHover: "It looks like you got this link without scanning a QR code or being redirected! What kind of sorcery is that?",
         status: "active",
     },
-    {
+    linktree: {
         name: "Legacy Linktree",
         source: "legacy",
-        query: "linktree",
         displayText: "You got here from the Legacy Linktree site!",
         extLink: "https://links.augystudios.com/",
         descHover: "Wow, you're a legacy! Click to get back there.",
         status: "active",
     },
-    {
+    tid: {
         name: "GFTV Team ID",
         source: "gftv-team-id",
-        query: "tid",
         displayText: "You scanned the QR code on Augy's Global Furry Television (GFTV) Team ID!",
         extLink: "https://globalfurry.tv/",
         descHover: "The GFTV Team ID is a unique identifier of every GFTV Team Member.",
         status: "active",
     },
-    {
+    "nc-4": {
         name: "Name Card 4",
         source: "namecard-4",
-        query: "nc-4",
         displayText: "You scanned the QR code on Augy's 4th iteration of his Name Card!",
         extLink: "https://augystudios.com/",
         descHover: "A furry name card is a personal creation that displays everything you need to know about the person you met. Augy's 4th name card features his new Telegram sticker pack's sticker.",
         status: "active",
     },
-    {
+    lifc24: {
         name: "LIFC 2024",
         source: "lifc24-conbadge",
-        query: "lifc24",
         displayText: "You scanned the NFC Tag on Augy's Little Island Furcon (LIFC) 2024 Conbadge!",
         extLink: "https://augystudios.com/",
         descHover: "LIFC is a furry convention held annually in Singapore.",
         status: "active",
     },
-    {
+    furum24: {
         name: "FurUM 2024",
         source: "furum24-conbadge",
-        query: "furum24",
         displayText: "You scanned the NFC Tag on Augy's Furs Upon Malaysia (FurUM) 2024 Conbadge!",
         extLink: "https://augystudios.com/",
         descHover: "FurUM is a furry convention held annually in Petaling Jaya Malaysia.",
         status: "inactive",
-    },
-]
+    }
+}
--- links/script.js
+++ links/script.js
@@ -24,63 +24,6 @@ let currentYear = currentDate.getFullYear();
 let currentYearElement = document.getElementById("currentYear")
 currentYearElement.textContent = currentYear;
 
-// Define the query list
-let queryList = [{
-        name: "Default",
-        source: "none",
-        query: "def",
-        displayText: "How did we get here?",
-        extLink: "https://augystudios.com/",
-        descHover: "It looks like you got this link without scanning a QR code or being redirected! What kind of sorcery is that?",
-        status: "active",
-    },
-    {
-        name: "Legacy Linktree",
-        source: "legacy",
-        query: "linktree",
-        displayText: "You got here from the Legacy Linktree site!",
-        extLink: "https://links.augystudios.com/",
-        descHover: "Wow, you're a legacy! Click to get back there.",
-        status: "active",
-    },
-    {
-        name: "GFTV Team ID",
-        source: "gftv-team-id",
-        query: "tid",
-        displayText: "You scanned the QR code on Augy's Global Furry Television (GFTV) Team ID!",
-        extLink: "https://globalfurry.tv/",
-        descHover: "The GFTV Team ID is a unique identifier of every GFTV Team Member.",
-        status: "active",
-    },
-    {
-        name: "Name Card 4",
-        source: "namecard-4",
-        query: "nc-4",
-        displayText: "You scanned the QR code on Augy's 4th iteration of his Name Card!",
-        extLink: "https://augystudios.com/",
-        descHover: "A furry name card is a personal creation that displays everything you need to know about the person you met. Augy's 4th name card features his new Telegram sticker pack's sticker.",
-        status: "active",
-    },
-    {
-        name: "LIFC 2024",
-        source: "lifc24-conbadge",
-        query: "lifc24",
-        displayText: "You scanned the NFC Tag on Augy's Little Island Furcon (LIFC) 2024 Conbadge!",
-        extLink: "https://augystudios.com/",
-        descHover: "LIFC is a furry convention held annually in Singapore.",
-        status: "active",
-    },
-    {
-        name: "FurUM 2024",
-        source: "furum24-conbadge",
-        query: "furum24",
-        displayText: "You scanned the NFC Tag on Augy's Furs Upon Malaysia (FurUM) 2024 Conbadge!",
-        extLink: "https://augystudios.com/",
-        descHover: "FurUM is a furry convention held annually in Petaling Jaya Malaysia.",
-        status: "inactive",
-    },
-]
-
 // Update the element with the id "textChange" based on query
 let textChange = document.getElementById("textChange")
 let textHover = document.getElementById("textHover")
