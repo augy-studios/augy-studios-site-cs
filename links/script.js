14930a4655f3abb90b699269d3f1388d168d6230
Augy <ateoks@gmail.com>
2024-04-20 01:59:38 +0800

    global links page test 85

--- links/script.js
+++ links/script.js
@@ -50,12 +50,20 @@ let queryList = {
         descHover: "The GFTV Team ID is a unique identifier of every GFTV Team Member.",
         status: "active",
     },
-    "nc-4": {
-        name: "Name Card 4",
-        source: "namecard-4",
-        displayText: "You scanned the QR code on Augy's 4th iteration of his Name Card!",
+    "nc-1": {
+        name: "Name Card 1",
+        source: "namecard-1",
+        displayText: "You scanned the QR code on Augy's 1st ever Name Card!",
+        extLink: "https://links.augystudios.com/augywolfy",
+        descHover: "A furry name card is a personal creation that displays everything you need to know about the person you met. Augy's 1st name card features his old fursona name and species, being a wolf.",
+        status: "active",
+    },
+    "nc-3": {
+        name: "Name Card 3",
+        source: "namecard-3",
+        displayText: "You scanned the QR code on Augy's 3rd iteration of his Name Card!",
         extLink: "https://augystudios.com/",
-        descHover: "A furry name card is a personal creation that displays everything you need to know about the person you met. Augy's 4th name card features his new Telegram sticker pack's sticker.",
+        descHover: "A furry name card is a personal creation that displays everything you need to know about the person you met. Augy's 3rd name card features his new Telegram sticker pack's sticker.",
         status: "active",
     },
     lifc24: {
