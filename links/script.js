32877d7b20dc4b18e335ca34e37b77a9e71fbd64
Augy <ateoks@gmail.com>
2024-05-12 03:30:19 +0800

    grammar fix 1

--- links/script.js
+++ links/script.js
@@ -79,7 +79,7 @@ let queryList = {
         source: "bizcard-1",
         displayText: "You scanned the QR code on the first ever Augy Studios business card!",
         extLink: "https://augystudios.com/",
-        descHover: "This card is also Augy's first time creating a translucent name card! Click to go to the Augy Studios website.",
+        descHover: "This is also Augy's first time creating a translucent name card! Click to go to the Augy Studios website.",
         status: "active",
     },
     "nc-1": {
